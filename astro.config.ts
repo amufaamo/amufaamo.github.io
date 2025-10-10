4573065f6b1bc6464177e82a60a7d8b427569ad2
Christine Tham <chris.tham@hellotham.com>
2025-10-10 11:10:49 +1100

    Change site url

--- astro.config.ts
+++ astro.config.ts
@@ -4,7 +4,7 @@ import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://christham.net',
+  site: 'https://hellotham.github.io',
   base: '/astro-base/',
   integrations: [UnoCSS(), sitemap()]
 })
