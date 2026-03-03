4b01ce7fb8991886b27c8c6b0d34476d76c81691
Masakazu Ishikawa/石川昌和/いしかわまさかず <45814539+amufaamo@users.noreply.github.com>
2026-03-03 12:42:53 +0900

    Modify site URL and enhance config comments
    Updated site URL and added comments for configuration.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,10 @@ import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  site: 'https://amufaamo.github.io',
-  // ここに sitemap() を追加します！順番はどこでも大丈夫です✨
+  // あなたのGitHubユーザー名に合わせてください
+  site: 'https://amufaamo.github.io', 
+  // リポジトリ名が「amufaamo.github.io」であれば '/' でOKです。
+  // もし別の名前のリポジトリなら '/リポジトリ名' にしてください。
+  base: '/', 
   integrations: [mdx(), sitemap(), tailwind()]
 });
